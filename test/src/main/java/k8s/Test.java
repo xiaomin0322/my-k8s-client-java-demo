ca478b7492756d7302d3d2269b27a50e02bbcbbd
zhangzengmin <xiaomin0322@sina.com>
2019-10-10 14:27:45 +0800

    init

---
+++ test/src/main/java/k8s/Test.java
@@ -0,0 +1,27 @@
+package k8s;
+
+import java.io.InputStream;
+
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.util.Config;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		String fileName = "classpath:/k8s/config";
+		InputStream inputStream = ResourceRenderer.resourceLoader(fileName);
+		ApiClient client = Config.fromConfig(inputStream);
+		// 五分钟
+		client.setConnectTimeout(5 * 60 * 1000);
+		Configuration.setDefaultApiClient(client);
+		CoreV1Api api = new CoreV1Api();
+		V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+		for (V1Pod item : list.getItems()) {
+			System.out.println(item.getMetadata().getName());
+		}
+	}
+}
